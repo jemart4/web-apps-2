3f91293c27087111dc6063a2584a1b6ea41ce6d5
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-02-20 01:34:01 +0000

    bitcoins

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,8 +1,29 @@
 class BitcoinsController < ApplicationController
 
   def index
+    
+    
     # add backend code
+    @bitcoin = params["amount"].to_f
+
+
+    require "net/http"
+    require "json"
+    @url = "https://blockchain.info/ticker"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @bitcoin_data = JSON.parse(@response)
+    # ----------------------
+  
+    # using the API, extract current rate of bitcoin in USD
+    @usd_rate = @bitcoin_data["USD"]["last"]
+  
+    # calculate value in USD of user's bitcoin
+    @usd_value = @usd_rate * @bitcoin
+
+
     # render bitcoins/index view
+
   end
 
 end
